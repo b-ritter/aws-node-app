428f0aa914f7b0e540f2043a1b7093df8f1f429f
b-ritter <ben@benjaminritter.com>
2018-01-13 07:41:55 -0600

    Show the port number in browser

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const app = express();
+// 8081 on aws, 5000 locally
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-   res.send('OK');
+   res.send(`OK: ${port}`);
   });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
